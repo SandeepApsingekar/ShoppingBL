10376fd0419d364adc238653b780777c378d5ee9
SandeepApsingekar <sandeep.apsingekar01@gmail.com>
2016-12-22 17:54:42 -0500

    Initial commit with code that calculates without offers. Includes Unit Test Cases

---
+++ CheckoutTest/CheckoutUnitTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingBL;
+
+namespace CheckoutTest
+{
+    [TestClass]
+    public class CheckoutUnitTest
+    {
+        [TestMethod]
+        public void TestTotalCostForApplesAndOranges()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 2, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 3, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 4, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 5, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 6, Name = "Orange", Price = 0.65m},
+                new Product() {Id = 7, Name = "Orange", Price = 0.65m},
+                new Product() {Id = 8, Name = "Orange", Price = 0.65m},
+                new Product() {Id = 9, Name = "Orange", Price = 0.65m},
+            };
+
+            var totalCost = new CheckoutService().GetTotalCostWithoutOffers(products);
+            
+            Assert.AreEqual(4.85m, totalCost);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAllApplesAndOrangesHaveSamePrice()
+        {
+            // We are testing whether one product type has different prices, then throw exception
+            // For example all apples should have same price, all oranges should have same price
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 2, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 3, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 4, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 5, Name = "Apple", Price = 0.55m},
+                new Product() {Id = 6, Name = "Orange", Price = 0.65m},
+                new Product() {Id = 7, Name = "Orange", Price = 0.65m},
+                new Product() {Id = 8, Name = "Orange", Price = 0.65m},
+                new Product() {Id = 9, Name = "Orange", Price = 0.65m},
+            };
+
+            var totalCost = new CheckoutService().GetTotalCostWithoutOffers(products);
+            //Expected apples + oranges prices =   1.35 + 1.95 = 3.30
+        }
+
+
+        [TestMethod]
+        public void TestWithZeroProductsToCheckout()
+        {
+            var products = new List<Product>();
+            var totalCost = new CheckoutService().GetTotalCostWithoutOffers(products);
+            Assert.AreEqual(0.0m, totalCost);
+        }
+
+        [TestMethod]
+        public void TestProductsOtherThanApplesOranges()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 2, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 3, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 4, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 5, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 6, Name = "Peach", Price = 0.65m},
+                new Product() {Id = 7, Name = "Peach", Price = 0.65m},
+                new Product() {Id = 8, Name = "Peach", Price = 0.65m},
+                new Product() {Id = 9, Name = "Peach", Price = 0.65m},
+            };
+
+            var totalCost = new CheckoutService().GetTotalCostWithoutOffers(products);
+            Assert.AreEqual(0.0m, totalCost);
+        }
+
+        [TestMethod]
+        public void TestSomeOtherProductsWithApplesOranges()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 2, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 3, Name = "Apple", Price = 0.45m},
+                new Product() {Id = 4, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 5, Name = "Banana", Price = 0.45m},
+                new Product() {Id = 6, Name = "Peach", Price = 0.65m},
+                new Product() {Id = 7, Name = "Orange", Price = 0.65m},
+                new Product() {Id = 8, Name = "Peach", Price = 0.65m},
+                new Product() {Id = 9, Name = "Peach", Price = 0.65m},
+            };
+
+            var totalCost = new CheckoutService().GetTotalCostWithoutOffers(products);
+            Assert.AreEqual(1.10m, totalCost);
+        }
+    }
+}
---
+++ ShoppingBL/CheckoutService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBL
+{
+    public class CheckoutService:ICheckout
+    {
+        public decimal GetTotalCostWithoutOffers(List<Product> products)
+        {
+            decimal totalCost = 0.0m;
+            int totalApples = 0;
+            decimal applePrice = 0.0m;
+            decimal orangePrice = 0.0m;
+            int totalOranges = 0;
+
+            // making a dictonary so that we can chek the uniqueness of the products and throw any exception if two keys are same
+            // i.e same product with different prices
+            var productPrice = products.GroupBy(p => new { p.Name, p.Price })
+                .ToDictionary(pd => pd.Key.Name, (pd) => (pd.Key.Price));
+
+            if (products.Any())
+            {
+                // we are assuming that customer put number of products (apples and oranges) he wants in the basket
+                // find total number of apples and oranges
+                // Right now the logic is checking for only apples and oranges, ideally these should come from a repository and should not be hard-coded
+                foreach (var product in products)
+                {
+                    if (product.Name == "Apple")
+                    {
+                        totalApples++;
+                    }
+                    if (product.Name == "Orange")
+                    {
+                        totalOranges++;
+                    }
+                }
+
+                foreach (var p in productPrice)
+                {
+                    if (p.Key == "Apple")
+                    {
+                        applePrice = p.Value;
+                    }
+                    if (p.Key == "Orange")
+                    {
+                        orangePrice = p.Value;
+                    }
+                }
+
+                totalCost += (totalApples * applePrice) + (totalOranges * orangePrice);
+            }
+
+            return totalCost;
+        }
+    }
+}
